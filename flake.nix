835e1147e986f42c19723dc122b89c576019a1d7
Joshua Gilman <joshuagilman@gmail.com>
2023-12-28 13:59:50 -0800

    chore: adds flake configuration

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Development Machine";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          name = "Development Machine";
+          packages = with pkgs; [
+            # Misc
+            mkdocs
+
+            # Python
+            poetry
+            python312
+          ];
+          enterShell = ''
+            echo "Welcome to the dev-machine development environment!"
+          '';
+        };
+      };
+    };
+}
